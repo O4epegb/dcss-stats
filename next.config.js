748749c85e3e942c28add9ac94711bf69cd842ee
Daniil Demidovich <daniil.demidovich@gmail.com>
2021-08-01 16:41:56 +0200

    Add api rewrite, update deps

--- next.config.js
+++ next.config.js
@@ -1,26 +1,17 @@
-// const rootUrl =
-//   process.env.NEXT_PUBLIC_ROOT_URL || 'https://stage.example.com';
-
 const withTM = require('next-transpile-modules')(['lodash-es']);
+
+const rootUrl = process.env.NEXT_PUBLIC_ROOT_URL;
 
 module.exports = withTM({
   eslint: {
     ignoreDuringBuilds: true,
   },
-  // async rewrites() {
-  //   return [
-  //     ...(process.env.NODE_ENV === 'development'
-  //       ? [
-  //           {
-  //             source: '/api/:slug*',
-  //             destination: `${rootUrl}/api/:slug*`,
-  //           },
-  //           {
-  //             source: '/uploads/:slug*',
-  //             destination: `${rootUrl}/uploads/:slug*`,
-  //           },
-  //         ]
-  //       : []),
-  //   ].filter(Boolean);
-  // },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:slug*',
+        destination: `${rootUrl}/api/:slug*`,
+      },
+    ].filter(Boolean);
+  },
 });
