93ca54c232096a986f93744f1ea84ea2d7fa34cc
Daniil Demidovich <daniil.demidovich@gmail.com>
2021-09-17 12:02:42 +0200

    Move loader image

--- src/components/Loader.tsx
+++
@@ -1,9 +0,0 @@
-import refreshSvg from '@refresh.svg';
-
-export const Loader = () => {
-  return (
-    <div className="flex items-center justify-center">
-      <div className="w-5 h-5 animate-spin" style={{ backgroundImage: `url(${refreshSvg.src})` }} />
-    </div>
-  );
-};
---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,9 @@
+import img from './refresh.svg';
+
+export const Loader = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <div className="w-5 h-5 animate-spin" style={{ backgroundImage: `url(${img.src})` }} />
+    </div>
+  );
+};
