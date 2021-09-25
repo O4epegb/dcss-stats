0cf925a27ca7acfa846e4555545fc8531eaecddf
Daniil Demidovich <daniil.demidovich@gmail.com>
2021-09-25 12:48:53 +0200

    Fix game tooltip playerName

--- src/screens/Player/Stats.tsx
+++ src/screens/Player/Stats.tsx
@@ -110,6 +110,8 @@ export const List = ({ items }: { items: [string, ReactNode][] }) => (
 );
 
 const GameTooltip: FC<{ game: Game }> = ({ game, children }) => {
+  const { player } = usePlayerPageContext();
+
   return (
     <HeadlessTooltip
       interactive
@@ -117,7 +119,7 @@ const GameTooltip: FC<{ game: Game }> = ({ game, children }) => {
       offset={[0, 0]}
       render={(attrs) => (
         <ul tabIndex={-1} className="max-w-[375px]" {...attrs}>
-          <GameItem shadow game={game} playerName={game.player?.name} />
+          <GameItem shadow game={game} playerName={player.name} />
         </ul>
       )}
     >
