f5d6577503b256153fe643e0417d5e738398bc2c
Daniil Demidovich <daniil.demidovich@gmail.com>
2021-11-14 15:08:07 +0100

    Move active streak on top

--- src/screens/Player/Streaks.tsx
+++ src/screens/Player/Streaks.tsx
@@ -1,5 +1,6 @@
 import { Fragment, useState } from 'react';
 import { last, first, orderBy } from 'lodash-es';
+import clsx from 'clsx';
 import { Game } from '@types';
 import { api } from '@api';
 import { addS, date, formatNumber } from '@utils';
@@ -8,12 +9,15 @@ import { List } from './Stats';
 import { usePlayerPageContext } from './context';
 
 type StreakGame = Pick<Game, 'id' | 'isWin' | 'endAt' | 'char'>;
+type StreakGroups = Array<StreakGame[]>;
 
 export const Streaks = () => {
-  const { streaks, player } = usePlayerPageContext();
-  const [streakGroups, setStreakGroups] = useState<Array<StreakGame[]>>([]);
+  const { streaks, player, lastGames } = usePlayerPageContext();
+  const [streakGroups, setStreakGroups] = useState<StreakGroups>([]);
   const [isVisible, setIsVisible] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+
+  const isOnStreak = lastGames[0]?.isWin && lastGames[1]?.isWin;
 
   return (
     <section className="space-y-1">
@@ -27,6 +31,7 @@ export const Streaks = () => {
             'Has no streaks of wins yet'
           )}
         </h2>
+        {isOnStreak && <div className="text-sm text-green-500">On streak right now!</div>}
       </header>
       {streaks.total > 0 && (
         <div className="flex items-center gap-x-4 gap-y-2 text-sm whitespace-nowrap">
@@ -55,7 +60,7 @@ export const Streaks = () => {
               setIsLoading(true);
 
               api
-                .get<{ streaks: { streaks: Array<StreakGame[]> } }>(`/players/${player.id}/streaks`)
+                .get<{ streaks: { streaks: StreakGroups } }>(`/players/${player.id}/streaks`)
                 .then((res) => {
                   setStreakGroups(res.data.streaks.streaks);
                   setIsVisible(true);
@@ -77,51 +82,56 @@ export const Streaks = () => {
       {isLoading && <Loader />}
       {isVisible && (
         <div className="space-y-2">
-          {orderBy(streakGroups, (streak) => streak.filter((x) => x.isWin).length, 'desc').map(
-            (streak, index) => {
-              const isActive = streak.every((x) => x.isWin);
-              const lastGame = last(streak);
-              const firstGame = first(streak);
-              const breaker = lastGame && !lastGame.isWin;
-              const streakLength = isActive ? streak.length : streak.length - 1;
+          {orderBy(
+            streakGroups,
+            [(streak) => last(streak)?.isWin, (streak) => streak.filter((x) => x.isWin).length],
+            ['desc', 'desc'],
+          ).map((streak, index) => {
+            const isActive = streak.every((x) => x.isWin);
+            const firstGame = first(streak);
+            const lastGame = last(streak);
+            const streakLength = isActive ? streak.length : streak.length - 1;
 
-              return (
-                <div
-                  key={index}
-                  className="text-sm py-1 px-2 border rounded border-gray-200 bg-white text-black"
-                >
-                  <div>
-                    <span className="font-medium">{streakLength} wins:</span>{' '}
-                    <span>
-                      {streak
-                        .filter((game) => game.isWin)
-                        .map((game, index) => (
-                          <Fragment key={game.id}>
-                            {index !== 0 && ', '}
-                            {game.char}
-                          </Fragment>
-                        ))}
-                    </span>
-                  </div>
-                  {breaker && lastGame && (
-                    <div>
-                      <span className="font-light">Streak breaker:</span> {lastGame.char}
-                    </div>
-                  )}
-                  {firstGame && (
-                    <div className="text-gray-400 text-xs pt-0.5">
-                      From <span>{date(firstGame.endAt).format('DD MMM YYYY, HH:mm:ss')}</span>{' '}
-                      {breaker && lastGame && (
-                        <>
-                          to <span>{date(lastGame.endAt).format('DD MMM YYYY, HH:mm:ss')}</span>
-                        </>
-                      )}
-                    </div>
-                  )}
+            return (
+              <div
+                key={index}
+                className={clsx(
+                  'text-sm py-1 px-2 border rounded border-gray-200 bg-white text-black',
+                  isActive && 'border-l-2 border-l-green-500',
+                )}
+              >
+                {isActive && <div className="text-green-500">Active streak</div>}
+                <div>
+                  <span className="font-medium">{streakLength} wins:</span>{' '}
+                  <span>
+                    {streak
+                      .filter((game) => game.isWin)
+                      .map((game, index) => (
+                        <Fragment key={game.id}>
+                          {index !== 0 && ', '}
+                          {game.char}
+                        </Fragment>
+                      ))}
+                  </span>
                 </div>
-              );
-            },
-          )}
+                {!isActive && lastGame && (
+                  <div>
+                    <span className="font-light">Streak breaker:</span> {lastGame.char}
+                  </div>
+                )}
+                {firstGame && (
+                  <div className="text-gray-400 text-xs pt-0.5">
+                    From <span>{date(firstGame.endAt).format('DD MMM YYYY, HH:mm:ss')}</span>{' '}
+                    {!isActive && lastGame && (
+                      <>
+                        to <span>{date(lastGame.endAt).format('DD MMM YYYY, HH:mm:ss')}</span>
+                      </>
+                    )}
+                  </div>
+                )}
+              </div>
+            );
+          })}
         </div>
       )}
     </section>
