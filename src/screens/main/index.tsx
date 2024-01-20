70fe2eb46d0c8d680b15a1ec33df7ceacf23fd5c
Daniil Demidovich <daniil.demidovich@gmail.com>
2024-01-20 16:31:01 +0100

    Add tournament banner

--- src/screens/main/index.tsx
+++ src/screens/main/index.tsx
@@ -39,6 +39,28 @@ export const MainPage = (props: Props) => {
           query={query}
           setQuery={setQuery}
         />
+        <a
+          href="https://crawl.develz.org/tournament/0.31/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex items-center justify-center gap-2 bg-[#282020] p-2 rounded text-white text-center text-lg border-violet-400 border-4"
+        >
+          The v0.31 Tournament will last until 08:00PM Sunday 04 February UTC
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6 shrink-0"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+            />
+          </svg>
+        </a>
         <Stats {...props} onLinkClick={onLinkClick} />
 
         <footer className="grid justify-between gap-1 text-xs text-gray-400 md:grid-cols-2">
