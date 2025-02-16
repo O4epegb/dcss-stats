7ae5a7ea11cac1e1e3cbfce0910399429db4e364
Daniil Demidovich <daniil.demidovich@gmail.com>
2025-02-16 14:25:04 +0100

    Update support page

--- apps/web/src/app/support/page.tsx
+++ apps/web/src/app/support/page.tsx
@@ -2,7 +2,7 @@ import { fetchApi } from '~/api/server'
 import { Logo } from '~/components/Logo'
 import { SupportersCurrentResponse } from '~/types'
 
-export const revalidate = 0
+export const revalidate = 30
 
 const SupportPage = async () => {
   const res = await fetchApi('/supporters/current')
@@ -18,11 +18,57 @@ const SupportPage = async () => {
         <Logo />
       </header>
 
-      <div className="w-full max-w-lg space-y-2">
-        <h2 className="text-lg font-semibold">Support</h2>
-        <div>
-          Current monthly goal: <span className="text-amber-400">${data.total}</span> of{' '}
-          <span className="text-amber-400">${data.goal}</span>
+      <div className="mx-auto max-w-3xl space-y-8 px-4 pt-0 md:pt-8">
+        <div className="space-y-4 text-center">
+          <h2 className="flex items-center justify-center gap-3 text-3xl font-bold">
+            Support DCSS Stats App
+          </h2>
+          <div className="text-zinc-500 dark:text-zinc-400">
+            Help us keep the data crunching and the servers running
+          </div>
+          <div className="text-xl">
+            Current monthly goal:{' '}
+            <span className="text-amber-500 dark:text-amber-400">${data.total}</span> of{' '}
+            <span className="text-amber-500 dark:text-amber-400">${data.goal}</span>
+          </div>
+        </div>
+        <div className="space-y-4 rounded border border-zinc-500 p-6">
+          <div className="flex flex-col space-y-2">
+            <h3 className="text-2xl font-semibold leading-none">Why Support Us?</h3>
+            <p className="text-sm text-zinc-500 dark:text-zinc-400">
+              Your support directly contributes to:
+            </p>
+          </div>
+          <ul className="m-0 list-disc space-y-2 pl-6">
+            <li>Server and infrastructure costs</li>
+            <li>Development of new features and improvements to existing ones</li>
+            <li>Maintenance and updates to keep everything running smoothly</li>
+          </ul>
+        </div>
+        <div className="flex flex-col items-center justify-center gap-4 rounded border-zinc-500">
+          <h3 className="text-2xl font-semibold leading-none">Methods</h3>
+          <div>
+            <a
+              target="_blank"
+              href="https://www.buymeacoffee.com/totalnoob"
+              rel="noreferrer"
+              className="block w-full rounded-full bg-[#ffdd00] px-6 py-3 font-medium text-black transition-all hover:bg-[#ffc800]"
+            >
+              Support on <b>Buy Me a Coffee</b>
+            </a>
+          </div>
+        </div>
+        <div className="space-y-2 text-center text-zinc-500 dark:text-zinc-400">
+          <p>Thank you for your contribution!</p>
+          <p className="text-sm">
+            Code source on{' '}
+            <a
+              className="font-bold hover:underline"
+              href="https://github.com/O4epegb/dcss-stats-app"
+            >
+              GitHub
+            </a>
+          </p>
         </div>
       </div>
     </div>
--- apps/web/src/screens/main/index.tsx
+++ apps/web/src/screens/main/index.tsx
@@ -33,22 +33,11 @@ export const MainPage = (props: Props) => {
 
           <div className="flex gap-5">
             <Link
-              target="_blank"
-              rel="noopener noreferrer"
               className="group relative hidden flex-col items-center justify-center sm:flex"
-              href="https://www.buymeacoffee.com/totalnoob"
+              href="/support"
             >
               <span className="flex items-center justify-center gap-1 text-xs group-hover:underline sm:text-base">
                 Support
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
-                  <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
-                </svg>
               </span>
               {data && (
                 <span className="absolute top-full text-nowrap text-xs text-gray-400">
