f840f375efe4ab8dfaec63a91146f952eb225f80
Daniil Demidovich <daniil.demidovich@gmail.com>
2025-03-22 19:55:12 +0100

    Update tsconfig module, add progress bar

--- apps/web/src/app/providers.tsx
+++ apps/web/src/app/providers.tsx
@@ -1,11 +1,19 @@
 'use client'
 
+import { ProgressProvider } from '@bprogress/next/app'
 import { ThemeProvider } from 'next-themes'
 
 export const Providers: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
     <ThemeProvider disableTransitionOnChange enableColorScheme enableSystem attribute="class">
-      {children}
+      <ProgressProvider
+        shallowRouting
+        height="4px"
+        color="rgb(180 83 9)"
+        options={{ showSpinner: false }}
+      >
+        {children}
+      </ProgressProvider>
     </ThemeProvider>
   )
 }
