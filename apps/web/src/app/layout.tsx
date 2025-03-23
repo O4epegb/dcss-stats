5a9a77b1cf306278e90d2ee1b62d77cff0fc642f
Daniil Demidovich <daniil.demidovich@gmail.com>
2025-03-23 12:18:36 +0100

    Fix analytics

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import '~/styles/globals.css'
-import { Analytics } from '@vercel/analytics/react'
+import { Analytics } from '@vercel/analytics/next'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import Script from 'next/script'
 import { Providers } from './providers'
