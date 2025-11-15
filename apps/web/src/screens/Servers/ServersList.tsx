f897cc2e8195afa1b6bea5aeafd3e0eba12810d0
Daniil Demidovich <daniil.demidovich@gmail.com>
2025-11-15 14:51:10 +0100

    Change cache again (seems like it's bugged with multiple components in file)

---
+++ apps/web/src/screens/Servers/ServersList.tsx
@@ -0,0 +1,84 @@
+import { orderBy } from 'lodash-es'
+import { fetchApi } from '~/api/server'
+import { Logfile, Server } from '~/types'
+import { pluralize, date, formatNumber } from '~/utils'
+
+export const ServersList = async () => {
+  'use cache'
+
+  const { servers }: { servers: Array<Server & { logfile: Array<Logfile> }> } = await fetchApi(
+    '/servers',
+  ).then((r) => r.json())
+
+  return (
+    <>
+      <h2 className="text-lg font-semibold">
+        Tracking {servers.length} {pluralize('server', servers.length)}:
+      </h2>
+
+      {orderBy(
+        servers.map((server) => {
+          const totalGames = server.logfile.reduce((acc, item) => acc + item.games, 0)
+
+          return {
+            ...server,
+            totalGames,
+          }
+        }),
+        (server) => server.totalGames,
+        'desc',
+      ).map((server) => {
+        return (
+          <div key={server.id} className="rounded border px-2 py-1">
+            <div className="flex justify-between">
+              <a
+                href={server.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="white-nowrap overflow-hidden text-ellipsis hover:underline"
+              >
+                <span className="font-medium">{server.abbreviation}</span> - {server.url}
+              </a>
+              <div className="whitespace-nowrap">
+                {formatNumber(server.totalGames)} {pluralize('game', server.totalGames)}
+              </div>
+            </div>
+            <details>
+              <summary>
+                {server.logfile.length} {pluralize('logfile', server.logfile.length)}
+              </summary>
+              <ul className="text-sm">
+                {server.logfile
+                  .slice()
+                  .sort((a, b) => b.version.localeCompare(a.version, undefined, { numeric: true }))
+                  .map((file) => {
+                    return (
+                      <li
+                        key={file.path}
+                        className="flex justify-between px-1 hover:bg-gray-100 dark:hover:bg-zinc-700"
+                        title={
+                          file.lastFetched && `Last fetched: ${date(file.lastFetched).format()}`
+                        }
+                      >
+                        <a
+                          href={server.baseUrl + file.path}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="hover:underline"
+                        >
+                          {file.path}
+                        </a>
+                        <div className="whitespace-nowrap">
+                          {formatNumber(file.games)} {pluralize('game', file.games)}
+                        </div>
+                      </li>
+                    )
+                  })}
+              </ul>
+            </details>
+          </div>
+        )
+      })}
+    </>
+  )
+}
