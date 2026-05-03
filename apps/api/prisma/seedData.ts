a0aee604da2c73b6951e121e0a551a485d8036b5
Daniil Demidovich <daniil.demidovich@gmail.com>
2026-05-03 17:42:11 +0200

    Add CRG server

--- apps/api/prisma/seedData.ts
+++ apps/api/prisma/seedData.ts
@@ -176,6 +176,26 @@ export const data = {
   ],
   servers: [
     {
+      name: 'crawl.roguelikes.gg',
+      abbreviation: 'CRG',
+      url: 'https://crawl.roguelikes.gg',
+      baseUrl: 'https://crawl.roguelikes.gg',
+      morgueUrl: 'https://crawl.roguelikes.gg/morgue',
+      ttyrecUrl: 'https://crawl.roguelikes.gg/ttyrec',
+      logfiles: [
+        ...rangeToTrunk(30).map((version) => ({
+          path: `/meta/0.${version}/logfile`,
+          version: `0.${version}`,
+          morgueUrlPrefix: undefined,
+        })),
+        {
+          path: `/meta/git/logfile`,
+          version: 'git',
+          morgueUrlPrefix: undefined,
+        },
+      ],
+    },
+    {
       name: 'crawl-br.roguelikes.gg',
       abbreviation: 'CBRG',
       url: 'https://crawl-br.roguelikes.gg',
